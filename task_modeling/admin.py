12a058f0aa8cd8f2e34f24551e2b52182bc24b30
Pavel <pavel.eliseev.work@yandex.ru>
2025-02-11 23:01:46 +0300

    + edit task model + add user config model + add admins for task and user config models

--- task_modeling/admin.py
+++ task_modeling/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from task_modeling.models import Task, UserConfig
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'created_at', 'updated_at', 'status')
+    search_fields = ('name', 'status')
+    list_filter = ('status',)
+    list_editable = ('status',)
+    list_per_page = 10
+    ordering = ('-created_at',)
+    date_hierarchy = 'created_at'
+    readonly_fields = ('created_at', 'updated_at')
+
+
+@admin.register(UserConfig)
+class UserConfigAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'created_at', 'updated_at')
+    search_fields = ('user',)
+    list_filter = ('user',)
+    list_per_page = 10
+    ordering = ('-created_at',)
+    date_hierarchy = 'created_at'
+    readonly_fields = ('created_at', 'updated_at')
--- task_modeling/models.py
+++ task_modeling/models.py
@@ -1,4 +1,21 @@
+from django.contrib.auth.models import User
 from django.db import models
+
+
+class UserConfig(models.Model):
+    config = models.JSONField(default=dict, verbose_name='Config')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created at')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Updated at')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='User')
+
+    def __str__(self):
+        return f"Config for {self.user.username}"
+
+    class Meta:
+        verbose_name = 'User Config'
+        verbose_name_plural = 'User Configs'
+
+
 
 class Task(models.Model):
     class Action:
@@ -12,10 +29,15 @@ class Task(models.Model):
             (DELETE, 'Delete'),
         )
 
-    name = models.CharField(max_length=255)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    status = models.CharField(max_length=255, choices=Action.CHOICES_STATUSES, default=Action.CREATE)
+    name = models.CharField(max_length=255, verbose_name='Task name')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Created at')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='Updated at')
+    status = models.CharField(max_length=255, choices=Action.CHOICES_STATUSES, default=Action.CREATE,
+                              verbose_name='Status')
+    config = models.ForeignKey(UserConfig, on_delete=models.SET_NULL, null=True, verbose_name='Config')
+
+    def __str__(self):
+        return f"{self.name} with config id: {self.config_id}"
 
     class Meta:
         verbose_name = 'Task'
