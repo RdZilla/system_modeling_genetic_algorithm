87db1efc7ad690d65407d034349dfe6824f4750a
Pavel <pavel.eliseev.work@yandex.ru>
2025-03-26 23:57:48 +0300

    DIP-20, setup deploy to server

--- modeling_system_backend/settings.py
+++ modeling_system_backend/settings.py
@@ -118,7 +118,7 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = 'static/'
-# STATIC_ROOT = os.environ.get("STATIC_ROOT", default=os.path.join(BASE_DIR, 'static'))  # TODO: check
+STATIC_ROOT = os.environ.get("STATIC_ROOT", default=os.path.join(BASE_DIR, 'static'))
 
 MEDIA_ROOT = os.environ.get("MEDIA_ROOT", default=os.path.join(BASE_DIR, 'media'))
 
