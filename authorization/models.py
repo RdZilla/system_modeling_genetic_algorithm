f2829f4f7023412035e9bb056a638c1886f8ab24
Pavel <pavel.eliseev.work@yandex.ru>
2025-04-03 02:10:23 +0300

    DIP-26, Модуль регистрации пользователей

--- authorization/models.py
+++ authorization/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils import timezone
+from datetime import timedelta
 
-# Create your models here.
+class EmailVerificationCode(models.Model):
+    email = models.EmailField()
+    code = models.CharField(max_length=6)  # например, 6-значный код
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def is_not_expired(self):
+        """Проверка, истек ли срок действия кода (10 минут)."""
+        expiration_time = self.created_at + timedelta(minutes=5)  # Время истечения кода
+        remaining_time = (expiration_time - timezone.now()).total_seconds()
+
+        return max(0, int(remaining_time)) if remaining_time > 0 else False
+
+    @classmethod
+    def clean_expired_codes(cls):
+        """Очищает все истекшие коды подтверждения."""
+        cls.objects.filter(created_at__lt=timezone.now() - timedelta(minutes=5)).delete()
+
+    def __str__(self):
+        return f"Code for {self.email}"
+
+    class Meta:
+        verbose_name = 'Код подтверждения электронной почты'
+        verbose_name_plural = 'Коды подтверждения электронной почты'
--- authorization/urls.py
+++ authorization/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import RegisterView, EmailTokenObtainPairView
+from .views import RegisterView, EmailTokenObtainPairView, SendCodeView
 from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path('register', RegisterView.as_view(), name='register'),
+    path('send-code', SendCodeView.as_view(), name='send-code'),
     path('login', EmailTokenObtainPairView.as_view(), name='login'),
     path('refresh', TokenRefreshView.as_view(), name='refresh'),
 ]
